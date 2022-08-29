8bf30928469e06e1dd2deec9aec68e9aede9fe9d
SiyaMajali <majalidsiya@gmail.com>
2022-08-29 17:57:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main()
+        {
+            System.Console.WriteLine("This is basic functionality code by Siyasanga Majali");
+        }
+    }
+}
